92be48a7c69ccf5811c8a9f8f57431aaaf08a0d5
iheb <shady.20mars@gmail.com>
2021-09-27 14:34:31 +0100

    v2.2

--- Middlware/auth.js
+++ Middlware/auth.js
@@ -3,7 +3,7 @@ let auth = (req, res, next) => {
   let n = d.getDay();
   let h = d.getHours();
 
-  if (n >= 1 && n <= 7 && h >= 9 && h <= 17) {
+  if (n >= 1 && n <= 5 && h >= 9 && h <= 12) {
     next();
   } else {
     res.send("<h1>BACK TOMORROW</h1>");
